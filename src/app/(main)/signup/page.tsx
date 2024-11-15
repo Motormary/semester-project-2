c94945f397dc330971639ee0896e9f02fc240941
Mathias <mat.moen@gmail.com>
2024-11-15 16:00:28 +0100

    reset

--- src/app/(main)/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignupForm from "./form"
-
-export default async function Page() {
-  return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <SignupForm />
-    </div>
-  )
-}
