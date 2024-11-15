597f47983587d5e43f4e9e2898de29235c817101
Mathias <mat.moen@gmail.com>
2024-11-15 20:26:08 +0100

    updated dep+eslint

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export default function removeEmptyObjValues(obj: any) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, value]) => value !== null),
+  )
+}
