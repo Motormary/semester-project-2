65b9702de1df1e67234e9e6edca008ff71da8ed0
Mathias <mat.moen@gmail.com>
2024-11-27 08:46:16 +0100

    font update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
       maxWidth: {
         content: "1440px",
       },
