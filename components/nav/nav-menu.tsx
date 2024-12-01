99009d110d67e72470e0b3159e8b097d5a299d29
Motormary <mat.moen@gmail.com>
2024-12-01 01:36:40 +0100

    update styles for nav

--- components/nav/nav-menu.tsx
+++ components/nav/nav-menu.tsx
@@ -118,10 +118,7 @@ export default async function NavMenu() {
                   href={`/vendors/${user.name}/bids`}
                 >
                   Bids{" "}
-                  <UserBidsCounter
-                    name={user.name}
-                    className="p-0.5 text-xs"
-                  />
+                  <UserBidsCounter name={user.name} className="p-0.5 text-xs" />
                 </Link>
               </DropdownMenuItem>
               <DropdownMenuItem asChild>
@@ -183,95 +180,100 @@ export default async function NavMenu() {
             />
           </Button>
         </SheetTrigger>
-        <SheetContent className="top-[65px] max-h-[calc(100%-64px)] w-full pt-0">
-          <SheetHeader className="sr-only">
-            <SheetTitle>Mobile Menu</SheetTitle>
-            <SheetDescription>Navigation</SheetDescription>
-          </SheetHeader>
-          <div className="my-2 flex flex-col gap-2">
-            {user ? (
+        <SheetContent className="top-[65px] max-h-[calc(100%-64px)] w-full pt-0 grid items-stretch overflow-y-auto">
+          <div>
+            <SheetHeader className="sr-only">
+              <SheetTitle>Mobile Menu</SheetTitle>
+              <SheetDescription>Navigation</SheetDescription>
+            </SheetHeader>
+            <div className="my-2 flex flex-col gap-2">
+              {user ? (
+                <SheetTrigger asChild>
+                  <Link
+                    href={"/vendors/user "}
+                    className="flex items-center gap-4 py-2 text-left sm:self-center"
+                  >
+                    <Avatar className="max-h-10 max-w-10">
+                      <AvatarImage
+                        height={40}
+                        width={40}
+                        src={user.avatar.url}
+                        alt="Avatar"
+                      />
+                      <AvatarFallback>
+                        <User />
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="overflow-hidden truncate whitespace-nowrap">
+                      <p className="text-sm font-semibold">{user.name}</p>
+                      <p className="truncate text-sm">{user.email}</p>
+                      <p className="text-sm text-muted-foreground">
+                        Total credits: {user.credits} Ω
+                      </p>
+                    </div>
+                  </Link>
+                </SheetTrigger>
+              ) : (
+                <div className="mx-auto flex w-full flex-col gap-6 py-4 sm:w-1/2">
+                  <SheetTrigger asChild>
+                    <Link
+                      href="/register"
+                      className={cn(buttonVariants({ variant: "default" }))}
+                    >
+                      Sign up
+                    </Link>
+                  </SheetTrigger>
+                  <SheetTrigger asChild>
+                    <Link
+                      href="/login"
+                      className={cn(buttonVariants({ variant: "outline" }))}
+                    >
+                      Sign in
+                    </Link>
+                  </SheetTrigger>
+                </div>
+              )}
+            </div>
+            <Separator />
+            <div className="flex flex-col items-center gap-6 py-4 [&>a]:flex [&>a]:justify-between [&>a]:max-sm:w-full [&>a]:sm:w-1/2">
               <SheetTrigger asChild>
-                <Link
-                  href={"/vendors/user "}
-                  className="flex items-center gap-4 py-2 text-left sm:self-center"
-                >
-                  <Avatar className="max-h-10 max-w-10">
-                    <AvatarImage
-                      height={40}
-                      width={40}
-                      src={user.avatar.url}
-                      alt="Avatar"
-                    />
-                    <AvatarFallback>
-                      <User />
-                    </AvatarFallback>
-                  </Avatar>
-                  <div className="overflow-hidden truncate whitespace-nowrap">
-                    <p className="text-sm font-semibold">{user.name}</p>
-                    <p className="truncate text-sm">{user.email}</p>
-                    <p className="text-sm text-muted-foreground">
-                      Total credits: {user.credits} Ω
-                    </p>
-                  </div>
+                <Link className="py-2" href={"/"}>
+                  <p>Home</p>
+                  <Home strokeWidth={1.5} className="size-5" />
                 </Link>
               </SheetTrigger>
-            ) : (
-              <div className="mx-auto flex w-full flex-col gap-6 py-4 sm:w-1/2">
-                <SheetTrigger asChild>
-                  <Link
-                    href="/register"
-                    className={cn(buttonVariants({ variant: "default" }))}
-                  >
-                    Sign up
-                  </Link>
-                </SheetTrigger>
-                <SheetTrigger asChild>
-                  <Link
-                    href="/login"
-                    className={cn(buttonVariants({ variant: "outline" }))}
-                  >
-                    Sign in
-                  </Link>
-                </SheetTrigger>
-              </div>
-            )}
-          </div>
-          <Separator />
-          <div className="flex flex-col items-center gap-6 py-4 [&>a]:flex [&>a]:justify-between [&>a]:max-sm:w-full [&>a]:sm:w-1/2">
-            <SheetTrigger asChild>
-              <Link className="py-2" href={"/"}>
-                <p>Home</p>
-                <Home strokeWidth={1.5} className="size-5" />
-              </Link>
-            </SheetTrigger>
-            {user ? (
-              <>
-                <SheetTrigger asChild>
-                  <Link className="py-2" href={`/vendors/${user.name}/bids`}>
-                    <p>Bids</p>
-                    <UserBidsCounter className="p-0.5 text-xs" name={user.name} />
-                  </Link>
-                </SheetTrigger>
-                <SheetTrigger asChild>
-                  <Link className="py-2" href={`/vendors/${user.name}/wins`}>
-                    <p>Wins</p>
-                    <span className="p-0.5 text-sm">{user._count.wins}</span>
-                  </Link>
-                </SheetTrigger>
-                {/*                 <SheetTrigger asChild>
+              {user ? (
+                <>
+                  <SheetTrigger asChild>
+                    <Link className="py-2" href={`/vendors/${user.name}/bids`}>
+                      <p>Bids</p>
+                      <UserBidsCounter
+                        className="p-0.5 text-xs"
+                        name={user.name}
+                      />
+                    </Link>
+                  </SheetTrigger>
+                  <SheetTrigger asChild>
+                    <Link className="py-2" href={`/vendors/${user.name}/wins`}>
+                      <p>Wins</p>
+                      <span className="p-0.5 text-sm">{user._count.wins}</span>
+                    </Link>
+                  </SheetTrigger>
+                  {/*                 <SheetTrigger asChild>
                   <Link className="py-2" href={"/vendors/user/bids"}>
                     <p>Vendors</p>
                     <Users strokeWidth={1.5} className="size-5" />
                   </Link>
                 </SheetTrigger> */}
-                <Separator />
-                <SheetTrigger className="h-full w-full">
-                  <MobileLogoutButton />
-                </SheetTrigger>
-              </>
-            ) : null}
+                  <Separator />
+                  <SheetTrigger className="h-full w-full">
+                    <MobileLogoutButton />
+                  </SheetTrigger>
+                </>
+              ) : null}
+            </div>
           </div>
-          <SheetFooter className="absolute bottom-5 left-1/2 w-[95%] -translate-x-1/2 px-5 sm:w-1/2">
+          <SheetFooter className="mx-auto w-full sm:w-1/2">
             <SheetClose asChild>
               <Button variant="outline">Close</Button>
             </SheetClose>
