8cd737e88c19473fb4d1fa822a0c3195b4db1ec8
Mathias <mat.moen@gmail.com>
2024-12-10 14:26:37 +0100

    pusher update link

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -12,7 +12,7 @@ export const pusherServer = new PusherServer({
 
 export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
   cluster: 'eu',
-  authEndpoint: '/api/pusher-auth',
+  authEndpoint: 'https://mkm-semester-project-2.netlify.app/api/pusher-auth',
   authTransport: 'ajax',
   auth: {
     headers: {
