ab260bd9de22a8271410f7dae48d1a253b6024e4
Mathias <mat.moen@gmail.com>
2024-12-10 12:16:05 +0100

    removed generateMetaData

--- app/(pages)/vendors/[...slug]/layout.tsx
+++ app/(pages)/vendors/[...slug]/layout.tsx
@@ -3,36 +3,10 @@ import ProfileInfo from "@/components/profile/profile-info"
 import { ProfileNav } from "@/components/profile/profile-nav"
 import { ReactNode, Suspense } from "react"
 import { ProfileLoading } from "./loading"
-import { Metadata } from "next"
-import { getUser } from "@/app/actions/user/get"
 
 type props = {
   params: Promise<{ slug: string[] }>
   children: ReactNode
-}
-
-export async function generateMetadata(
-  { params }: props,
-): Promise<Metadata> {
-  const slug = (await params).slug
-  
-  const { data, success } = await getUser(slug[0])
-
-
-  if (!success) {
-    return {
-      title: 'Profile Not Found',
-      description: 'The requested profile could not be found.',
-    }
-  }
-
-  return {
-    title: data.data.name,
-    description: data.data?.bio ?? "EBOX user profile, track users activity",
-    openGraph: {
-      images: [data.data.avatar.url]
-    }
-  }
 }
 
 export default async function Layout({
